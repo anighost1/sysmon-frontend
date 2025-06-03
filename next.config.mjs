0550439c78aa5167f6e26f1a3bed50cec905d722
Anil Tigga <anil.anil300@gmail.com>
2025-06-04 02:30:36 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-import nextPWA from 'next-pwa';
-
-const withPWA = nextPWA({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-});
 
 const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
